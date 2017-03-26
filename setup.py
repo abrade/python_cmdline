4a4cc42a0d0d10987778fb1354ddc26f188c7774
Alexander Brade <alex.brade@gmx.de>
2017-03-26 16:17:39 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import distutils.core as _core
+
+_core.setup(
+    name='cmdline',
+    version='0.1',
+    description='Cmdline tools to create better cmdline applications',
+    author='Alexander Brade',
+    author_email='alex.brade@gmx.de',
+    url='',
+    packages=[
+    ],
+    py_modules=[
+    ]
+)
